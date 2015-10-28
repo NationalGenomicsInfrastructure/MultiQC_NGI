7610db4d502876744620d45065a0a61da4d027a1
Phil Ewels <phil.ewels@scilifelab.se>
2015-10-28 18:18:22 +0100

    Made demo hook function better.

--- multiqc_ngi/hooks.py
+++ multiqc_ngi/hooks.py
@@ -2,10 +2,13 @@
 """ MultiQC hook functions - we tie into the MultiQC
 core here to add in extra functionality. """
 
-from multiqc import (logger, __version__)
+import logging
 from multiqc.utils import (report, config)
-from multiqc.utils.log import init_log, LEVELS
+
+log = logging.getLogger('multiqc')
 
 def after_modules():
   """ Plugin code to run when MultiQC modules have completed  """
-  print("MultiQC NGI plugin demonstration - {} modules repoted!".format(len(report.modules_output)))
+  num_modules = len(report.modules_output)
+  status_string = "MultiQC hook - {} modules repoted!".format(num_modules)
+  log.critical(status_string)
