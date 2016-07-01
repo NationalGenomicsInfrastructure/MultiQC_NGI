45cd5c4e8ca088cc17474b1c7263cb1e3b102c61
Phil Ewels <phil.ewels@scilifelab.se>
2016-07-02 00:31:41 +0200

    Moved heatmap above MDS plot.

--- multiqc_ngi/modules/ngi_rnaseq/ngi_rnaseq.py
+++ multiqc_ngi/modules/ngi_rnaseq/ngi_rnaseq.py
@@ -10,8 +10,8 @@ import re
 from multiqc import config, BaseMultiqcModule
 
 # Import the NGI-RNAseq submodules
-from . import mds_plot
 from . import heatmap
+from . import mds_plot
 
 # Initialise the logger
 log = logging.getLogger('multiqc.modules.ngi_rnaseq')
@@ -32,13 +32,13 @@ class MultiqcModule(BaseMultiqcModule):
         n = dict()
         
         # Call submodule functions
-        n['mds_plot'] = mds_plot.parse_reports(self)
-        if n['mds_plot'] > 0:
-            log.info("Found {} MDS plots".format(n['mds_plot']))
-        
         n['heatmaps'] = heatmap.parse_reports(self)
         if n['heatmaps'] > 0:
             log.info("Found {} heatmaps".format(n['heatmaps']))
+        
+        n['mds_plot'] = mds_plot.parse_reports(self)
+        if n['mds_plot'] > 0:
+            log.info("Found {} MDS plots".format(n['mds_plot']))
         
         # Exit if we didn't find anything
         if sum(n.values()) == 0:
