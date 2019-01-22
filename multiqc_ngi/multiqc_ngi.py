b568a5828441b0942f76cf5840e33925a1f9b8ae
Phil Ewels <phil.ewels@scilifelab.se>
2019-01-22 14:58:28 +0100

    Log that we got the statusdb config file ok

--- multiqc_ngi/multiqc_ngi.py
+++ multiqc_ngi/multiqc_ngi.py
@@ -527,11 +527,13 @@ class ngi_metadata():
             conf_file = os.path.join(os.environ.get('HOME'), '.ngi_config', 'statusdb.yaml')
             with open(conf_file, "r") as f:
                 sdb_config = yaml.load(f)
+                log.debug("Got MultiQC_NGI statusdb config from the home directory.")
         except IOError:
             log.debug("Could not open the MultiQC_NGI statusdb config file {}".format(conf_file))
             try:
                 with open(os.environ['STATUS_DB_CONFIG'], "r") as f:
                     sdb_config = yaml.load(f)
+                    log.debug("Got MultiQC_NGI statusdb config from $STATUS_DB_CONFIG: {}".format(os.environ['STATUS_DB_CONFIG']))
             except (KeyError, IOError):
                 log.debug("Could not get the MultiQC_NGI statusdb config file from env STATUS_DB_CONFIG")
                 log.warn("Could not find a statusdb config file")
